84b0f7d8d22c100a0bd91a96b0d0617145dab131
BinarHugo <binarcomcom@gmail.com>
2021-08-23 21:31:12 +0300

    3.3. Разбор первой программы. [#159033]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args){
+        System.out.println("Saveliy Borisich");
+        System.out.println("15.07.1999");
+    }
+
+}
